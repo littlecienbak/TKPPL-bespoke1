ea0e46fe813ea93db2190a506e729b28f147a8e3
agus <agus.leonardo30336@gmail.com>
2019-11-16 21:46:04 +0700

    Ganti tema slide

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Cara install:
 2. Ketik "npm install -g generator-bespoke"
 3. Ketik "npm install -g yo"
 4. Ketik "npm install -g gulp-cli"
-5. Ketik "gulp serve"
-6. Buka browser, kemudian ketik "localhost:8080" untuk menampilkannya
+5. Ketik "npm install"
+6. Ketik "gulp serve"
+7. Buka browser, kemudian ketik "localhost:8080" untuk menampilkannya
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,18 @@
-// Require Node modules in the browser thanks to Browserify: http://browserify.org
-var bespoke = require('bespoke');
-var classes = require('bespoke-classes');
-var nav = require('bespoke-nav');
-var scale = require('bespoke-scale');
-var bullets = require('bespoke-bullets');
-var hash = require('bespoke-hash');
-var prism = require('bespoke-prism');
-var multimedia = require('bespoke-multimedia');
-var extern = require('bespoke-extern');
+var bespoke = require('bespoke'),
+  buildWars = require('bespoke-theme-build-wars'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale'),
+  progress = require('bespoke-progress'),
+  backdrop = require('bespoke-backdrop');
 
-// Bespoke.js
-bespoke.from({ parent: 'article.deck', slides: 'section' }, [
-  classes(),
-  nav(),
+bespoke.from('article', [
+  buildWars(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
   scale(),
-  bullets('.build, .build-items > *:not(.build-items)'),
-  hash(),
-  prism(),
-  multimedia(),
-  extern(bespoke)
+  progress(),
+  backdrop()
 ]);
